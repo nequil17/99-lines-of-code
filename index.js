9516820702fdb16e12c852d403a072d74871f182
InnovateBham_13 <InnovateBham_13@Admins-iMac.local>
2017-06-16 15:48:15 -0700

    ...

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var friends = [ "Brian", "Kelsey", "Mike", "Lou", "Brittney" ];
 for(var i = 0; i < friends.length; i++) {
-
+console.log (friends[i] + ':')
     for(var j = 99; j >= 1; j--) {
         if (j > 2) {
             console.log([j] + ' lines of code in the file, ' + [j] + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + (j-1) + ' lines of code in the file' );
